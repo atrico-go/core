cfeaca3297f73d5025a83d631a05214665149e70
Rob <rob.rimmer@mclaren.com>
2021-05-11 15:36:24 +0100

    WaitWithTimeout removed (not idiomatic)

--- syncEx/with_timeout.go
+++ syncEx/with_timeout.go
@@ -3,15 +3,7 @@ package syncEx
 import (
 	"context"
 	"sync"
-	"time"
 )
-
-// Perform action or timeout
-// return true if action completed, false if timed out
-func WithTimeout(action func(), timeout time.Duration) bool {
-	ctx,_ := context.WithTimeout(context.Background(), timeout)
-	return WithContext(action, ctx) == nil
-}
 
 // Perform action with context (cancel and/or timeout)
 // return nil if action completed, error if timed out/cancelled
@@ -29,19 +21,11 @@ func WithContext(action func(), ctx context.Context) error {
 	}
 }
 
-// Wait for a WaitGroup with a timeout
-func WaitWithTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
-	return WithTimeout(func() { wg.Wait() }, timeout)
-}
 // Wait for a WaitGroup with a context
 func WaitWithContext(wg *sync.WaitGroup, ctx context.Context) error {
 	return WithContext(func() { wg.Wait() }, ctx)
 }
 
-// Lock a Mutex with a timeout
-func LockWithTimeout(m *sync.Mutex, timeout time.Duration) bool {
-	return WithTimeout(func() { m.Lock() }, timeout)
-}
 // Lock a Mutex with a context
 func LockWithContext(m *sync.Mutex, ctx context.Context) error {
 	return WithContext(func() { m.Lock() }, ctx)
